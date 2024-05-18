4e61684c03ce143de2fc5974c7980f9793c14525
eparlato <edoardoparlato@nebulab.com>
2024-05-18 16:48:24 +0200

    Do not send any birthday greeting if no user born today is present

--- spec/send_greetings_use_case_spec.rb
+++ spec/send_greetings_use_case_spec.rb
@@ -8,8 +8,8 @@ require "email_birthday_notifier"
 require "calendar"
 
 RSpec.describe SendGreetingsUseCase do
-  context "when the input has one person whose birthday is today" do
-    it "sends a birthday message" do
+  context "when the data source has one person whose birthday is today" do
+    it "sends a birthday greeting message" do
       user_file_data_source = instance_double("UserFileDataSource")
       email_birthday_notifier = instance_double("EmailBirthdayNotifier")
       calendar = instance_double("Calendar")
@@ -24,6 +24,24 @@ RSpec.describe SendGreetingsUseCase do
       send_greetings_use_case.send_greetings
       
       expect(email_birthday_notifier).to have_received(:send_greeting_message_to).with(person)
+    end
+  end
+  
+  context "when the data source has one user and today is NOT its birthday" do
+    it "doesn't send any birthday greeting message" do
+      user_file_data_source = instance_double("UserFileDataSource")
+      email_birthday_notifier = instance_double("EmailBirthdayNotifier")
+      calendar = instance_double("Calendar")
+
+      allow(calendar).to receive(:get_today_date).and_return(Date.new(2024, 10, 8))
+      allow(user_file_data_source).to receive(:find_users_born_on).with(10, 8).and_return([])
+      allow(email_birthday_notifier).to receive(:send_greeting_message_to)
+
+      send_greetings_use_case = SendGreetingsUseCase.new(user_file_data_source, email_birthday_notifier, calendar)
+
+      send_greetings_use_case.send_greetings
+      
+      expect(email_birthday_notifier).not_to have_received(:send_greeting_message_to)
     end
   end
 end
